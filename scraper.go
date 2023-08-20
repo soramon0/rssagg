26554bda7ffb9a1d9eee6aa3b50a9dc5d031ec55
soramon0 <23435312+soramon0@users.noreply.github.com>
2023-08-20 19:32:38 +0100

    save scraped posts into db

--- scraper.go
+++ scraper.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"context"
+	"database/sql"
 	"log"
+	"strings"
 	"sync"
 	"time"
 
+	"github.com/google/uuid"
 	"github.com/soramon0/rssagg/internal/database"
 )
 
@@ -40,8 +43,37 @@ func scrape(db *database.Queries, concurrency int, timeBetweenRequest time.Durat
 				}
 
 				for _, item := range rss.Channel.Item {
-					log.Println("Found post", item.Title, "on feed", feed.Name)
+					description := sql.NullString{}
+					if item.Description != "" {
+						description.String = item.Description
+						description.Valid = true
+					}
+
+					pubDate, err := time.Parse(time.RFC1123Z, item.PubDate)
+					if err != nil {
+						log.Printf("couldn't parse date %v with err %v\n", item.PubDate, err)
+						continue
+					}
+
+					_, err = db.CreatePost(context.Background(), database.CreatePostParams{
+						ID:          uuid.New(),
+						Url:         item.Link,
+						Title:       item.Title,
+						Description: description,
+						PublishedAt: pubDate,
+						FeedID:      feed.ID,
+						CreatedAt:   time.Now().UTC(),
+						UpdatedAt:   time.Now().UTC(),
+					})
+					if err != nil {
+						if strings.Contains(err.Error(), `violates unique constraint "posts_url_key"`) {
+							continue
+						}
+						log.Println("failed to create post for:", item.Title, item.Link)
+						log.Println(err)
+					}
 				}
+
 				log.Printf("Feed %s collected, %v posts found\n", feed.Name, len(rss.Channel.Item))
 			}(wg, db, f)
 		}
